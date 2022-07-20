18bf660934f05aa7faf6b8913f172322a99a3f3f
LelielC <82486785+LelielC@users.noreply.github.com>
2022-07-20 16:59:45 -0500

    Create install.R

---
+++ install.R
@@ -0,0 +1,6 @@
+install.packages("tidyverse")
+install.packages("rmarkdown")
+install.packages("httr")
+install.packages("shinydashboard")
+install.packages('leaflet')
+install.packages('FactoClass')
